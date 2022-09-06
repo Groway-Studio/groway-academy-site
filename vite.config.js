094cb8245dd415404e40f6d6768b5c219b47ed57
joel ibaceta <mail@joelibaceta.com>
2022-09-06 17:12:39 +0900

    update env

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 /** @type {import('vite').UserConfig} */
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+
+
+export default defineConfig(({ command, mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: env.APP_ENV
+    }
+  }
+
 })
