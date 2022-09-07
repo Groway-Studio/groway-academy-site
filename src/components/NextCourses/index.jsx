6bb174af49949fd440cd3ce0285b55a0188183d7
cristiandelcidd <cristian_delcid10@yahoo.com>
2022-09-06 19:48:35 -0600

    width updated and styles fixes

--- src/components/NextCourses/index.jsx
+++ src/components/NextCourses/index.jsx
@@ -12,11 +12,11 @@ const NextCourses = () => {
       <div className={styles.next_courses__content}>
         <div>
           <img src={PythonLogo} alt="Python" draggable={false} />
-          <h3>Python de 0 a Experto</h3>
+          <h3>Aprende Python como se debe...</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-            nobis illum, eaque deserunt eveniet illo? Sunt provident hic tempora
-            atque corrupti nesciunt.
+            Este curso de Python es para que te puedas introducir y repasar los
+            fundamentos de programación a través de Python de una forma
+            sencilla, eficiente y divertida.
           </p>
 
           <a
@@ -27,15 +27,7 @@ const NextCourses = () => {
           </a>
         </div>
         <div>
-          <img src={NodeLogo} alt="Node" draggable={false} />
-          <h3>Node de 0 a Experto</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-            nobis illum, eaque deserunt eveniet illo? Sunt provident hic tempora
-            atque corrupti nesciunt.
-          </p>
-
-          <a>Más Info</a>
+          <p>Próximamente...</p>
         </div>
       </div>
     </section>
